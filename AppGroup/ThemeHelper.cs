0c6190981a1b78707d59ebd5e66815e88aa9a309
iandiv <dvineian1239@gmail.com>
2026-04-23 22:39:09 +0800

    fixed theme helper for proper theme change

--- AppGroup/ThemeHelper.cs
+++ AppGroup/ThemeHelper.cs
@@ -10,16 +10,25 @@ namespace AppGroup {
         public static void UpdateTitleBarColors(Window window) {
             if (window.Content is FrameworkElement root) {
                 root.ActualThemeChanged += (sender, args) => {
-                    var titleBar = window.AppWindow.TitleBar;
-                    var isDarkMode = (window.Content as FrameworkElement)?.ActualTheme == ElementTheme.Dark;
-
-                    titleBar.ButtonForegroundColor = isDarkMode ? Colors.White : Colors.Black;
+                    ApplyTitleBarColors(window);
                 };
-
-                var initialIsDarkMode = (window.Content as FrameworkElement)?.ActualTheme == ElementTheme.Dark;
-                var initialTitleBar = window.AppWindow.TitleBar;
-                initialTitleBar.ButtonForegroundColor = initialIsDarkMode ? Colors.White : Colors.Black;
+                ApplyTitleBarColors(window);
             }
+        }
+
+        public static void ApplyTitleBarColors(Window window) {
+            var titleBar = window.AppWindow.TitleBar;
+            var isDark = (window.Content as FrameworkElement)?.ActualTheme == ElementTheme.Dark;
+
+            titleBar.ButtonForegroundColor = isDark ? Colors.White : Colors.Black;
+            titleBar.ButtonHoverForegroundColor = isDark ? Colors.White : Colors.Black;
+            titleBar.ButtonHoverBackgroundColor = isDark
+                ? Windows.UI.Color.FromArgb(30, 255, 255, 255)
+                : Windows.UI.Color.FromArgb(30, 0, 0, 0);
+            titleBar.ButtonPressedForegroundColor = isDark ? Colors.White : Colors.Black;
+            titleBar.ButtonPressedBackgroundColor = isDark
+                ? Windows.UI.Color.FromArgb(50, 255, 255, 255)
+                : Windows.UI.Color.FromArgb(50, 0, 0, 0);
         }
     }
 }
